1d04e278728993d0fa7d7203b5b50512335af66a
George Omondi <georgeomondi1963@gmail.com>
2022-09-09 05:42:15 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print string in the put function
+ *
+ * Description: using the main function
+ * this program prints "program is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"program is like building a multiligual puzzle");
+	return (0);
+}
