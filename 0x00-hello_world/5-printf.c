beee081497b4c17451d98d051ab19dcbdb7d216a
George Omondi <georgeomondi1963@gmail.com>
2022-09-09 06:22:00 -0700

    Hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 
 /**
-
-* main - Prints a text
-
-*
-
-* Return: Always 0 (Success)
-
-*/
-
+ * main - print the string in the put function
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-printf("with proper grammar, but the outcome is a piece of
-
-art,\n");
-
-return (0);
-
+	printf("with proper grammar, but outcome is a piece of art,\n");
+	return (0);
 }
+
