a895ac3cd9c7db6a5d9a1f49424b6e66f8f95850
George Omondi <georgeomondi1963@gmail.com>
2022-09-20 10:59:34 -0700

    git push "

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+
+ *_strlen - returns the length of a string
+
+ * @s: string
+
+ *Return: returns length as integer;
+
+ */
+
+
+
+int _strlen(char *s)
+
+{
+
+	int len = 0;
+
+
